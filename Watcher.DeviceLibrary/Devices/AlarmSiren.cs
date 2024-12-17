b57a0b78815e4cf7b0d2775966be433560fdedfd
Libor Masek <libormasek@hotmail.com>
2024-12-17 17:00:27 +0800

    Added AlarmSiren device

---
+++ Watcher.DeviceLibrary/Devices/AlarmSiren.cs
@@ -0,0 +1,21 @@
+﻿namespace Watcher.DeviceLibrary.Devices;
+
+public class AlarmSiren : IDevice
+{
+    public string Id { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Type => "Alarm";
+
+    public string SoundFile { get; set; } = string.Empty;
+    public int Volume { get; set; } = 50;
+
+    public Task TurnOn()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task TurnOff()
+    {
+        return Task.CompletedTask;
+    }
+}
